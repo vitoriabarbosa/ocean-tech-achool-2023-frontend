769b71f71153d8747a260f7b4bfdaa59b41014d0
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2023-09-26 00:11:53 +0000

    Conteúdo HTML e CSS movido para componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ function App() {
   return (
     <>
       <Card />
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-
-        <div className='tags'>
-          <span className='tag'>Status: Vivo</span>
-          <span className='tag'>Espécie: Humana</span>
-          <span className="tag">Origem: Terra C-137</span>
-        </div>
-
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez"/>
-      </div>
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,15 @@
 import "./Card.css"
 
 export default function Card() {
-    return <div>Card</div>
+    return <div className="card">
+        <h2>Rick Sanchez</h2>
+
+        <div className='tags'>
+          <span className='tag'>Status: Vivo</span>
+          <span className='tag'>Espécie: Humana</span>
+          <span className="tag">Origem: Terra C-137</span>
+        </div>
+
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez"/>
+      </div>
 }
